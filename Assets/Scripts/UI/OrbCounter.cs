35dec6870f6af7d1a10f7a3a8084132a0b571c3e
DESKTOP-F3CVV0E\Mitchell <1005780@student.sae.edu.au>
2017-04-10 21:46:15 +1000

    - Completed Orb tracker

--- Assets/Scripts/UI/OrbCounter.cs
+++ Assets/Scripts/UI/OrbCounter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OrbCounter : MonoBehaviour
 {
@@ -10,13 +11,29 @@ public class OrbCounter : MonoBehaviour
     public GameObject PassBar;
     public GameObject CompletionBar;
     public RectTransform Notches;
+    public GameObject TrackerIcon;
 
     public List<GameObject> NotchList;
+
+    public Vector3 CompletionTargetScale;
+    public Vector3 TrackerTargetPosition;
+
+    public float BarLerpSpeed;
+
+    public Color ColourPerfect;
+    public Color ColourPass;
+    public Color ColourFail;
+    
 
 
 	// Use this for initialization
 	void Start ()
     {
+        //Set default targets
+        CompletionTargetScale = CompletionBar.GetComponent<RectTransform>().localScale;
+        TrackerTargetPosition = TrackerIcon.GetComponent<RectTransform>().localPosition;
+
+
         //Set the completion bar to 0 scale
         CompletionBar.transform.localScale = new Vector3(0,1,1);
 
@@ -25,7 +42,7 @@ public class OrbCounter : MonoBehaviour
 
         //Set the PassBar to the right scale based on pass score
         //Remainder of perfect score: 1 - PerfectScore/PassScore
-        PassBar.transform.localScale = new Vector3(1 - (float)PerfectScore / (float)PassScore, 1, 1);
+        PassBar.transform.localScale = new Vector3(1 - (float)PerfectScore  / (float)PassScore, 1, 1);
 
         //Create a bunch of notches to break up the bar based on pass score
         for (int i = 0; i < GameDirector.LevelManager.CurrentLevel.passScore + 1; i++)
@@ -47,6 +64,33 @@ public class OrbCounter : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        //Determine Target 
+        CompletionTargetScale.x = Mathf.Clamp01((float)GameDirector.LevelManager.CurrentLevel.orbsUsed / (float)GameDirector.LevelManager.CurrentLevel.passScore);
+        //Lerp To Target
+        CompletionBar.transform.localScale =  Vector3.Lerp(CompletionBar.GetComponent<RectTransform>().localScale,  CompletionTargetScale, BarLerpSpeed);
+
+        //Determine Target
+        TrackerTargetPosition.x = CompletionBar.GetComponent<RectTransform>().localPosition.x + (CompletionBar.GetComponent<RectTransform>().rect.width * CompletionBar.GetComponent<RectTransform>().localScale.x);
+        //Follow lerped target
+        TrackerIcon.GetComponent<RectTransform>().localPosition = TrackerTargetPosition;
+
+        UpdateColour();
+
+    }
+
+    void UpdateColour()
+    {
+        if(GameDirector.LevelManager.CurrentLevel.orbsUsed <= GameDirector.LevelManager.CurrentLevel.perfectScore)
+        {
+            TrackerIcon.GetComponent<Image>().color = ColourPerfect;
+        }
+        else if(GameDirector.LevelManager.CurrentLevel.orbsUsed <= GameDirector.LevelManager.CurrentLevel.passScore)
+        {
+            TrackerIcon.GetComponent<Image>().color = ColourPass;
+        }
+        else
+        {
+            TrackerIcon.GetComponent<Image>().color = ColourFail;
+        }
+    }
 }
